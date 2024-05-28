8d06f0781cc67545c1f301659dca4c3291776849
omarsolieman <omarmedhat2004@gmail.com>
2024-05-28 13:18:17 +0800

    Update 2004A2.py
    added tests passes 70%

--- 2004A2.py
+++ 2004A2.py
@@ -34,3 +34,30 @@ class OrfFinder:
                     substrings.append(substring)
         substrings.sort(key=lambda x: (len(x), x))
         return substrings
+
+genome1 = OrfFinder("AAABBBCCC")
+print(genome1.find("AAA", "BB"))  # Output: ['AAABB', 'AAABBB']
+print(genome1.find("BB", "A"))    # Output: []
+print(genome1.find("AA", "BC"))   # Output: ['AABBBC', 'AAABBBC']
+print(genome1.find("A", "B"))     # Output: ['AAAB', 'AAABB', 'AAABBB', 'AAB', 'AABB', 'AABBB', 'AB', 'ABB', 'ABBB']
+print(genome1.find("AA", "A"))    # Output: ['AAA']
+print(genome1.find("AAAB", "BBB"))  # Output: []
+
+genome2 = OrfFinder("ATGCATGCATGC")
+print(genome2.find("ATG", "TGC"))  # Output: ['ATGCATGC', 'ATGCATGCATGC']
+print(genome2.find("CAT", "ATG"))  # Output: ['CATGCATG']
+print(genome2.find("A", "A"))      # Output: ['ATGCA', 'ATGCATGCA']
+print(genome2.find("AT", "AT"))    # Output: []
+print(genome2.find("TGC", "ATG"))  # Output: ['TGCATG']
+
+genome3 = OrfFinder("AAAAAA")
+print(genome3.find("AAA", "AAA"))  # Output: ['AAAAAA']
+print(genome3.find("A", "A"))      # Output: ['AA', 'AAA', 'AAAA', 'AAAAA', 'AAAAAA']
+print(genome3.find("AAAA", "AA"))  # Output: ['AAAAAA']
+
+genome4 = OrfFinder("ATCGATCG")
+print(genome4.find("AT", "CG"))    # Output: ['ATCG', 'ATCGATCG']
+print(genome4.find("ATC", "TCG"))  # Output: ['ATCGATCG']
+print(genome4.find("C", "C"))      # Output: ['CG', 'CGATC']
+print(genome4.find("AT", "AT"))    # Output: []
+print(genome4.find("ATCG", "TCG"))  # Output: ['ATCGATCG']
